7ac4bf713098d2ed9d7de09e3b3ebabb2c38695b
LF <sinn6842@gmail.com>
2020-12-08 23:23:37 -0600

    Compilación App

---
+++ app/precache-manifest.3cd89770876ee7127c7786db6fd5c7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cb3724fb1faaedfa4d4e2e808d0cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e77ad9afba619646c8",
+    "url": "/static/css/2.910d6bac.chunk.css"
+  },
+  {
+    "revision": "9f073b885c13bc29329c",
+    "url": "/static/css/main.730401a9.chunk.css"
+  },
+  {
+    "revision": "c1e77ad9afba619646c8",
+    "url": "/static/js/2.2dc96e29.chunk.js"
+  },
+  {
+    "revision": "d5c109ee23cb02a50d9c3c2217b7caea",
+    "url": "/static/js/2.2dc96e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f073b885c13bc29329c",
+    "url": "/static/js/main.63385ce0.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  }
+]);
